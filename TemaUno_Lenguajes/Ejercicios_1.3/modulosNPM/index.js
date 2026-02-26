6b84b3a593de394d64e9d787cca4593b89efc071
Alan-C-Ake <colliakealanalberto@gmail.com>
2026-02-25 22:46:23 -0500

    Add files via upload

---
+++ TemaUno_Lenguajes/Ejercicios_1.3/modulosNPM/index.js
@@ -0,0 +1,13 @@
+//Alan Alberto Colli Ake
+//import generateName from 'sillyname';
+
+//const sillyName = generateName();
+
+//console.log(sillyName);
+
+import superheroes from 'superheroes';
+
+const hero = superheroes[Math.floor(Math.random() * superheroes.length)];
+
+console.log('Superhéroe aleatorio:');
+console.log(hero);
