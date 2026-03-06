fe5d8a98817de144cff35e9bff1aae89a1fec0c5
ALAN-C-AKE <colliakealanalberto@gmail.com>
2026-03-05 23:07:49 -0500

    Solución definitiva: archivos de Ejercicios_1.4 visibles

---
+++ TemaUno_Lenguajes/Ejercicios_1.4/Practica_1.4.3/index.js
@@ -0,0 +1,26 @@
+import bodyParser from "body-parser";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
+console. log(_dirname);
+
+const app = express ( );
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }) );
+
+app.get("/", (req, res) => {
+res.sendFile(_dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+console. log(req.body);
+res.send("Datos recibidos");
+});
+
+app. listen(port, () => {
+console. log(`Servidor ejecutandose en el puerto ${port}`);
+});
