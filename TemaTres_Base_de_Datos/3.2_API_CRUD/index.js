5260ea9d0ee958ebf4f28efbf5d839a3a077ab50
Alan-C-Ake <colliakealanalberto@gmail.com>
2026-05-04 11:58:32 -0500

    Actualizando index.js con CRUD Completo AA 3.2

--- TemaTres_Base_de_Datos/3.2_API_CRUD/index.js
+++ TemaTres_Base_de_Datos/3.2_API_CRUD/index.js
@@ -1,3 +1,4 @@
+// Alan Alberto Colli Ake 8-B
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
@@ -10,8 +11,8 @@ const puerto = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
+// Conexión a la base de datos
 const uri = process.env.uri;
 
 mongoose.connect(uri)
@@ -22,10 +23,12 @@ mongoose.connect(uri)
         console.error("Error al conectar a la base de datos:", error);
     });
 
+// Ruta de bienvenida
 app.get('/', (req, res) => {
     res.send('Bienvenido a mi API CRUD');
 });
 
+//Agregar usuarios (POST)
 app.post('/usuarios', async (req, res) => {
     try {
         const usuario = await Usuario.create(req.body);
@@ -36,6 +39,70 @@ app.post('/usuarios', async (req, res) => {
     }
 });
 
+//Obtener todos los usuarios (GET)
+app.get('/usuarios', async (req, res) => {
+    try {
+        const usuarios = await Usuario.find();
+        res.status(200).json(usuarios);
+    } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+        res.status(500).json({ error: 'Error al obtener los usuarios' });
+    }
+});
+
+//Consulta de un usuario por su ID (GET)
+app.get('/usuario/:id', async (req, res) => {
+    try {
+        const { id } = req.params; 
+        const usuario = await Usuario.findById(id);
+        
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+        
+        res.status(200).json(usuario);
+    } catch (error) {
+        console.error("Error al obtener el usuario:", error);
+        res.status(500).json({ error: 'Error al obtener el usuario' });
+    }
+});
+
+//Actualizar un usuario (PUT)
+app.put('/usuario/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const usuario = await Usuario.findByIdAndUpdate(id, req.body);
+        
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+        
+        const usuarioActualizado = await Usuario.findById(id);
+        res.status(200).json(usuarioActualizado);
+        console.log(usuarioActualizado);
+    } catch (error) {
+        console.error("Error al actualizar:", error);
+        res.status(500).json({ error: 'Error al actualizar el usuario' });
+    }
+});
+
+//Elimina un usuario (DELETE)
+app.delete('/usuario/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const usuario = await Usuario.findByIdAndDelete(id);
+        
+        if (!usuario) {
+            return res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+        
+        res.status(200).json({ message: 'Usuario eliminado' });
+    } catch (error) {
+        console.error("Error al eliminar:", error);
+        res.status(500).json({ error: 'Error al eliminar el usuario' });
+    }
+});
+
 app.listen(puerto, () => {
-    console.log(`Servidor escuchando en http://localhost:${puerto}`);
+    console.log(`Servidor Corriendo en http://localhost:${puerto}`);
 });
